bddd90e87d88730f3524088aa2b71e10ada98b06
Suma <kaysuma05@gmail.com>
2018-08-18 15:14:13 +0530

    Added Model for Books

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,19 +2,40 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let bookSchema = new Schema({
-    isbn:{
+    isbn: {
         type: String,
         trim: true,
         required: true
     },
-    name:{
+    name: {
         type: String,
         required: true,
-        trim:true
+        trim: true
     },
-    price:{
+    price: {
         type: Number,
         default: 0
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    b64: {
+        type: String,
+        required: false
+    },
+    uid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
     }
 });
 
